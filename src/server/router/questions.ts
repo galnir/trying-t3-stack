6971e0ae5c0cf9b943be228b7187fae05984e646
galnir <nirnir122@gmail.com>
2022-07-16 12:42:05 +0300

    create poll page

--- src/server/router/questions.ts
+++ src/server/router/questions.ts
@@ -31,11 +31,9 @@ export const questionRouter = createRouter()
     },
   })
   .mutation("create", {
-    input: z.object({
-      question: z.string().min(5).max(600),
-    }),
+    input: createQuestionValidator,
     async resolve({ input, ctx }) {
-      if (!ctx.token) return { error: "Unauthorized" };
+      if (!ctx.token) throw new Error("Unauthorized");
       return await prisma.pollQuestion.create({
         data: {
           question: input.question,
